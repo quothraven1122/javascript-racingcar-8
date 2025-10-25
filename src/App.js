9504a4453fdba1b19388a42e6039ed1a9ae4779c
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-10-25 11:26:21 +0900

    feat: add function to receive user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
+//입력 받는 함수
+const getInput = async () => {
+  const input = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  const tries = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+  //시도횟수, {name: i, count:0 }의 array 리턴
+  return [tries, input.split(",").map((i) => ({ name: i, count: 0 }))];
+};
+
 class App {
-  async run() {}
+  async run() {
+    const [tries, cars] = await getInput();
+  }
 }
 
 export default App;
